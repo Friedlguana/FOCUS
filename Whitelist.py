fd79f05d7084d4819403883996f98a7588d331c5
Fried <rijulravikumar@gmail.com>
2023-08-26 19:57:21 +0400

    Added video stream for face recognition

--- Whitelist.py
+++ Whitelist.py
@@ -1,76 +1,74 @@
 import cv2
-import numpy as np
+import face_recognition
 import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-import face_recognition
 
 # Load known face encodings and names
-arya_image = face_recognition.load_image_file(r"G:\SDC\FOCUS-main\FOCUS-main\accepted faces\ayra.jpg")
-arya_face_encoding = face_recognition.face_encodings(arya_image)[0]
+known_face_encodings = []
+known_face_names = ['Rijul', 'Ravi']
 
-known_face_encodings = [arya_face_encoding]
-known_face_names = ["Arya Mirani"]
+# Load the reference images for each face
+reference_image_1 = face_recognition.load_image_file('test.jpg')
+reference_face_encoding_1 = face_recognition.face_encodings(reference_image_1)[0]
+known_face_encodings.append(reference_face_encoding_1)
 
-# Initialize the video capture
-video_capture = cv2.VideoCapture(0)
+reference_image_2 = face_recognition.load_image_file('test1.jpg')
+reference_face_encoding_2 = face_recognition.face_encodings(reference_image_2)[0]
+known_face_encodings.append(reference_face_encoding_2)
 
+# Open the webcam
+cap = cv2.VideoCapture(0)  # Use 0 for default webcam, 1 if you have an external one
+
+tolerance = 0.5
+r = 0
+u = 0
 while True:
-    ret, frame = video_capture.read()
+    ret, frame = cap.read()
 
-    # Find face locations and encodings in the current frame
+    if not ret:
+        print("Error: Failed to capture a frame.")
+        break
+
     face_locations = face_recognition.face_locations(frame)
     face_encodings = face_recognition.face_encodings(frame, face_locations)
-    face_names = []
 
-    for face_encoding in face_encodings:
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=tolerance)
         name = "Unknown"
+        
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
+        
+        if name == 'Rijul':
+            r+=1
+            if r==10:
+                print('Rijul detected')
+                break  # Break when Rijul is detected
+        elif name == 'Unknown':
+            u += 1
+            if u == 10:
+                print('Unknown')
+                break  # Break when Unknown is detected
+            
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-        if any(matches):
-            name = known_face_names[matches.index(True)]
-
-        face_names.append(name)
-
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        if name != "Unknown":
-            os.startfile(r"G:\SDC\FOCUS-main\FOCUS-main\Whitelisted Ports.txt")
-
-        cv2.imshow('Video', frame)
-
-
-
-# Release the video capture and close all windows
-        video_capture.release()
+    cv2.imshow('Face Recognition', frame)
+    if u == 5:
         cv2.destroyAllWindows()
+        cv2.imwrite('photo.jpg', frame)
+        smtp_port = 587
+        smtp_server = "smtp.gmail.com"
 
-        if name == "Unknown":
-            cap = cv2.VideoCapture(0)
-            ret, frame = cap.read()
-            cap.release()
-            cv2.imwrite("G:\SDC\FOCUS-main\FOCUS-main\illegal logins\photo2.jpg", frame)
-
-    # SENDING AN EMAIL FOR UNAUTHORIZED ACCESS
-            smtp_port = 587                 
-            smtp_server = "smtp.gmail.com"  
-
-            email_from = "aryamirani06@gmail.com"
-            email_list = ["aryaamiranii@gmail.com", "28278.arya@iswkoman.com"]
-            pswd = "xbxskelbiclhyjcz" 
-            subject = "UNAUTHORIZED LOGIN ATTEMPT !!"
+        email_from = "aryamirani06@gmail.com"
+        email_list = ["rijulravikumar@gmail.com", 'aryaamiranii@gmail.com']
+        pswd = "xbxskelbiclhyjcz"
+        subject = "UNAUTHORIZED LOGIN ATTEMPT !!"
 
         def send_emails(email_list):
             for person in email_list:
@@ -83,7 +81,7 @@ while True:
 
                 msg.attach(MIMEText(body, 'plain'))
 
-                filename = "G:\SDC\FOCUS-main\FOCUS-main\illegal logins\photo2.jpg"
+                filename = "photo.jpg"
                 attachment = open(filename, 'rb')  # r for read and b for binary
                 attachment_package = MIMEBase('application', 'octet-stream')
                 attachment_package.set_payload((attachment).read())
@@ -103,4 +101,15 @@ while True:
             TIE_server.quit()
 
         send_emails(email_list)
-        quit()
+        break
+
+    if r == 5:
+        print('Rijul detected')
+        os.startfile(r"Whitelisted Ports.txt")
+        break
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
